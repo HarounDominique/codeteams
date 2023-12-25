d12714523e090990be08bb404835acd47d7b7e80
HAROUN <haroundominique@gmail.com>
2023-12-25 12:37:40 +0100

    first commit

---
+++ src/main/java/com/empresaurios/codeteams/CodeteamsApplication.java
@@ -0,0 +1,13 @@
+package com.empresaurios.codeteams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeteamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeteamsApplication.class, args);
+	}
+
+}
