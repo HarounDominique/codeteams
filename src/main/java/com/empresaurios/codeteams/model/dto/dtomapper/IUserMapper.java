d5988da415dfdaedc42ea3b42239ccab1bee36be
Angelmbx <mirandabl.angel@gmail.com>
2023-12-28 17:51:18 +0100

    mapper done

--- src/main/java/com/empresaurios/codeteams/model/dto/dtomapper/IUserMapper.java
+++ src/main/java/com/empresaurios/codeteams/model/dto/dtomapper/IUserMapper.java
@@ -1,7 +1,18 @@
 package com.empresaurios.codeteams.model.dto.dtomapper;
 
+import com.empresaurios.codeteams.model.Users;
+import com.empresaurios.codeteams.model.dto.UsersDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface IUserMapper {
+
+    IUserMapper INSTANCE = Mappers.getMapper(IUserMapper.class);
+    UsersDTO toDTO(Users user);
+    List<UsersDTO> toDTOList(List<Users> users);
+    Users toEntity(UsersDTO usersDTO);
+
 }
