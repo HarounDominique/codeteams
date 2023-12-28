30634d48cfccdcff517fd5307a27bf5b8b6a4bbd
Angelmbx <mirandabl.angel@gmail.com>
2023-12-28 19:15:55 +0100

    Added /add /get /getAll /update /delete to UsersController

--- src/main/java/com/empresaurios/codeteams/controller/UsersController.java
+++ src/main/java/com/empresaurios/codeteams/controller/UsersController.java
@@ -1,17 +1,39 @@
 package com.empresaurios.codeteams.controller;
 
+import com.empresaurios.codeteams.api.IUsersService;
+import com.empresaurios.codeteams.model.dto.UsersDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    private IUsersService usersService;
+
+
+
     @GetMapping
     public String testController(){
         return "Users controller works baby!";
     }
+
+    @PostMapping(value = "/get")
+    public UsersDTO queryUser (@RequestBody UsersDTO usersDTO) { return usersService.queryUser(usersDTO);}
+
+    @GetMapping(value ="/getAll")
+    public List<UsersDTO> queryAllUsers() { return usersService.queryAllUsers();}
+
+    @PostMapping(value = "/add")
+    public int addUser (@RequestBody UsersDTO usersDTO) { return usersService.insertUser(usersDTO);}
+
+    @PutMapping(value = "/update")
+    public int updateUser (@RequestBody UsersDTO usersDTO) { return usersService.updateUser(usersDTO);}
+
+    @DeleteMapping(value = "/delete")
+    public int deleteUser (@RequestBody UsersDTO usersDTO) { return usersService.deleteUser(usersDTO);}
 
 }
