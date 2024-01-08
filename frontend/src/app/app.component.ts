0e0760e0d9df0385a0bb1fc0f4ebf1ed64f377fb
HAROUN <haroundominique@gmail.com>
2024-01-08 17:21:40 +0100

    REFERENCE POINT

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,13 +8,6 @@ import { RouterOutlet } from '@angular/router';
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
-=======
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
->>>>>>> f3e9854fc62ed6020127ed4de30d0d8328ba0a81
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-=======
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
->>>>>>> f3e9854fc62ed6020127ed4de30d0d8328ba0a81
