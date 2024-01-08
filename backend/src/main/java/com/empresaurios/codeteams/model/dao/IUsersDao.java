6e011a715c157b526411280fdbb673ae68b8ac04
HAROUN <haroundominique@gmail.com>
2024-01-08 17:56:52 +0100

    backend - database conection succeded

--- backend/src/main/java/com/empresaurios/codeteams/model/dao/IUsersDao.java
+++ backend/src/main/java/com/empresaurios/codeteams/model/dao/IUsersDao.java
@@ -2,8 +2,8 @@ package com.empresaurios.codeteams.model.dao;
 
 import com.empresaurios.codeteams.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUsersDao extends JpaRepository<Users, Integer> {
-
-
 }
